9628c0567606ee31f7c1c4beea3d49f16937d734
yaman <yamankhaldi5@gmail.com>
2024-08-21 13:01:20 +0300

    first commit

---
+++ src/IsraeliQueue.java
@@ -0,0 +1,20 @@
+public class IsraeliQueue<E>  implements Iterable<E>
+{
+   private Node<E> head;
+   private Node<E> tail ;
+    private int  size;
+
+    public IsraeliQueue() {}
+
+    public void addPerson (E person , E friend )
+    {
+
+
+
+
+    }
+    public int returnSize(){
+        return size;
+    }
+}
+
